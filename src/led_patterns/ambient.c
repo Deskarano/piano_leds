f69ed1bd2b284c15122d9ca5bda939752a3f2461
Gregor Haas <gregorhaas1997@gmail.com>
2017-09-14 10:49:47 -0400

    Added documentation for ambient.c and ambient.h

--- src/led_patterns/ambient.c
+++ src/led_patterns/ambient.c
@@ -3,15 +3,19 @@
 
 #include <stdlib.h>
 
+/**
+ * Internal struct to hold all working data for the led_update_ambient_normal function
+ */
 typedef struct led_update_piano_ambient_data
 {
-    unsigned int last_color;
+    unsigned int last_color;      /**< The last color used by the led_update_piano_normal function*/
 } led_update_piano_ambient_data_t;
 
-void *new_led_update_ambient_normal_data()
+void *new_led_update_ambient_normal_data_t()
 {
     led_update_piano_ambient_data_t *ret = malloc(sizeof(led_update_piano_ambient_data_t));
 
+    //set everything to 0
     ret->last_color = 0;
     
     return ret;
@@ -23,10 +27,12 @@ void led_update_ambient_normal(led_update_function_data_t *data)
 
     if(color == 0)
     {
+        //generate a new color
         color = (unsigned int) random();
     }
     else
     {
+        //get a close color
         color = random_near_color(color,
                                   RAND_COLOR_THRESHOLD,
                                   RAND_COLOR_THRESHOLD,
@@ -35,11 +41,13 @@ void led_update_ambient_normal(led_update_function_data_t *data)
 
     for(int i = 0; i < LED_COUNT; i++)
     {
+        //introduce some randomness
         unsigned int adj_color = random_near_color(color,
                                                    RAND_COLOR_THRESHOLD * 3,
                                                    RAND_COLOR_THRESHOLD * 3,
                                                    RAND_COLOR_THRESHOLD * 3);
 
+        //copy color to led_states[]
         data->led_states[i] = adj_color;
     }
 }
--- src/led_patterns/ambient.h
+++ src/led_patterns/ambient.h
@@ -3,8 +3,18 @@
 
 #include "core.h"
 
-void *new_led_update_ambient_normal_data();
+/**
+ * Function to create and initialize a new led_update_ambient_normal_data_t
+ *
+ * @return A pointer to the new led_update_ambient_normal_data_t
+ */
+void *new_led_update_ambient_normal_data_t();
 
+/**
+ * The normal ambient LED function. Gently changes color of the entire strip over time
+ *
+ * @param data The filled out data struct for led_update_ambient_normal
+ */
 void led_update_ambient_normal(led_update_function_data_t *data);
 
 #endif //SMART_LEDS_LED_PATTERNS_AMBIENT_H
