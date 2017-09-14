71c9d3033cf5b5a71eee4ba723d233036ab28561
Gregor Haas <gregorhaas1997@gmail.com>
2017-09-14 10:42:47 -0400

    Minor changes

--- src/led_patterns/ambient.h
+++ src/led_patterns/ambient.h
@@ -5,6 +5,6 @@
 
 void *new_led_update_ambient_normal_data();
 
-void led_update_ambient_normal(led_update_function_data_t *);
+void led_update_ambient_normal(led_update_function_data_t *data);
 
 #endif //SMART_LEDS_LED_PATTERNS_AMBIENT_H
