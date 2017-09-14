9449f389560c129b16d2aa4ad5da3c85b71fc57c
Gregor Haas <gregorhaas1997@gmail.com>
2017-09-14 14:30:29 -0400

    Implemented led_update_ambient_gradient()

--- src/led_patterns/color_utils.h
+++ src/led_patterns/color_utils.h
@@ -1,6 +1,11 @@
 #ifndef SMART_LEDS_LED_PATTERNS_COLOR_UTILS_H
 #define SMART_LEDS_LED_PATTERNS_COLOR_UTILS_H
 
+unsigned char extract_red(unsigned int color);
+unsigned char extract_blue(unsigned int color);
+unsigned char extract_green(unsigned int color);
+unsigned int color_from_channels(unsigned char r, unsigned char b, unsigned char g);
+
 unsigned int normalize_color(unsigned int color, char max_intensity);
 unsigned int adjacent_color(unsigned int color, double factor);
 unsigned int random_near_color(unsigned int color, char dr_max, char db_max, char dg_max);
