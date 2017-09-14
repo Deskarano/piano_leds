75516bf3273efe47879597521592817c3780b0ff
Gregor Haas <gregorhaas1997@gmail.com>
2017-09-14 14:47:54 -0400

    Created some documentation for ambient.h

--- src/led_patterns/ambient.h
+++ src/led_patterns/ambient.h
@@ -9,6 +9,12 @@
  * @return A pointer to the new led_update_ambient_normal_data_t
  */
 void *new_led_update_ambient_normal_data_t();
+
+/**
+ * Function to create and initialize a new led_update_ambient_gradient_data_t
+ *
+ * @return A pointer to the new led_update_ambient_gradient_data_t
+ */
 void *new_led_update_ambient_gradient_data_t();
 
 /**
@@ -17,6 +23,12 @@ void *new_led_update_ambient_gradient_data_t();
  * @param data The filled out data struct for led_update_ambient_normal
  */
 void led_update_ambient_normal(led_update_function_data_t *data);
+
+/**
+ * The gradient ambient LED function. Changes the gradient color between the left and right ends of the strip over time
+ *
+ * @param data The filled out data struct for led_update_ambient_gradient
+ */
 void led_update_ambient_gradient(led_update_function_data_t *data);
 
 #endif //SMART_LEDS_LED_PATTERNS_AMBIENT_H
