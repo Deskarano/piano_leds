d20cd85755f4c41b82a3d1c9ecb98715a191a12a
Gregor Haas <gregorhaas1997@gmail.com>
2017-09-14 09:47:57 -0400

    Created documentation for piano submodule

--- src/led_patterns/piano.c
+++ src/led_patterns/piano.c
@@ -4,28 +4,36 @@
 
 #include <stdlib.h>
 
+/**
+ * Internal struct to hold all working data for the led_update_piano_normal function
+ */
 typedef struct led_update_piano_normal_data
 {
-    int key_pressed[LED_COUNT];
-    int key_sustain[LED_COUNT];
+    int key_pressed[LED_COUNT];         /**< Array storing whether a given piano key is pressed or not */
+    int key_sustain[LED_COUNT];         /**< Array storing whether to sustain a given key or not */
 
-    unsigned int last_color;
-    int sustain;
+    int sustain;                        /**< Only ever 1 or 0. Indicates if the sustain pedal is pressed or not */
+
+    unsigned int last_color;            /**< The last color used by the led_update_piano_normal function*/
 } led_update_piano_normal_data_t;
 
+/**
+ * Internal struct to hold all working data for the led_update_piano_war function
+ */
 typedef struct led_update_piano_war_data
 {
-    int occupied[LED_COUNT];
-    unsigned int colors[LED_COUNT];
-    int direction[LED_COUNT];
-    int size[LED_COUNT];
-    int locked[LED_COUNT];
+    int occupied[LED_COUNT];            /**< Array storing whether a given LED is occupied */
+    unsigned int colors[LED_COUNT];     /**< Array storing current colors of all LEDs */
+    int direction[LED_COUNT];           /**< Array storing which direction a given LED is "traveling" in */
+    int size[LED_COUNT];                /**< Array storing the size of a given LED */
+    int locked[LED_COUNT];              /**< Array controlling locking and unlocking of LEDs */
 } led_update_piano_war_data_t;
 
-void *new_led_update_piano_normal_data()
+void *new_led_update_piano_normal_data_t()
 {
     led_update_piano_normal_data_t *ret = malloc(sizeof(led_update_piano_normal_data_t));
 
+    //initialize everything to 0
     for(int i = 0; i < LED_COUNT; i++)
     {
         ret->key_pressed[i] = 0;
@@ -38,10 +46,11 @@ void *new_led_update_piano_normal_data()
     return ret;
 }
 
-void *new_led_update_piano_war_data()
+void *new_led_update_piano_war_data_t()
 {
     led_update_piano_war_data_t *ret = malloc(sizeof(led_update_piano_war_data_t));
 
+    //initialize everything to 0
     for(int i = 0; i < LED_COUNT; i++)
     {
         ret->occupied[i] = 0;
@@ -56,10 +65,12 @@ void *new_led_update_piano_war_data()
 
 void led_update_piano_normal(led_update_function_data_t *data)
 {
+    //copy pattern_data into a local variable for less typing
     led_update_piano_normal_data_t *pattern_data = (led_update_piano_normal_data_t *) data->pattern_data;
 
     if(pipe_size((pipe_generic_t *) data->data_pipe) > 0)
     {
+        //pick a color to use for all keys currently in the pipe
         unsigned int color = pattern_data->last_color;
 
         if(color == 0)
@@ -80,10 +91,12 @@ void led_update_piano_normal(led_update_function_data_t *data)
 
             if(data->buffer[0] == 144)
             {
+                //pop contained a new key, analyze its number and velocity
                 pipe_pop(data->data_pipe, data->buffer, 2);
 
                 if(data->buffer[1] > 0)
                 {
+                    //keys that are hit harder create brighter LEDs
                     data->led_states[data->buffer[0] - 21] = normalize_color(color, data->buffer[1] * (char) 2);
                     pattern_data->key_pressed[data->buffer[0] - 21] = 1;
                     pattern_data->key_sustain[data->buffer[0] - 21] = 1;
@@ -98,8 +111,7 @@ void led_update_piano_normal(led_update_function_data_t *data)
                     }
                 }
             }
-
-            if(data->buffer[0] == 176)
+            else if(data->buffer[0] == 176)
             {
                 pipe_pop(data->data_pipe, data->buffer, 2);
 
@@ -117,7 +129,7 @@ void led_update_piano_normal(led_update_function_data_t *data)
         }
     }
 
-    //if not sustaining, remove any keys from sustain[] that are not key_pressed[]
+    //if not sustaining, remove any keys from key_sustain[] that are not key_pressed[]
     if(!pattern_data->sustain)
     {
         for(int i = 0; i < LED_COUNT; i++)
@@ -130,7 +142,7 @@ void led_update_piano_normal(led_update_function_data_t *data)
         }
     }
 
-    //sustain logic
+    //fade out sustained LEDs slower than non-sustained LEDs
     for(int i = 0; i < LED_COUNT; i++)
     {
         if(!(pattern_data->key_sustain[i]))
@@ -146,6 +158,7 @@ void led_update_piano_normal(led_update_function_data_t *data)
 
 void led_update_piano_war(led_update_function_data_t *data)
 {
+    //copy pattern_data into a local variable for less typing
     led_update_piano_war_data_t *pattern_data = (led_update_piano_war_data_t *) data->pattern_data;
 
     //unlock all values
@@ -161,15 +174,18 @@ void led_update_piano_war(led_update_function_data_t *data)
             if((i == 0 && pattern_data->direction[i] == -1) ||
                (i == LED_COUNT - 1 && pattern_data->direction[i] == 1))
             {
+                //corner impact cases
                 pattern_data->size[i]--;
 
                 if(pattern_data->size[i])
                 {
+                    //LED is alive; change color
                     pattern_data->colors[i] = random_near_color(pattern_data->colors[i],
-                                                                   RAND_COLOR_THRESHOLD,
-                                                                   RAND_COLOR_THRESHOLD,
-                                                                   RAND_COLOR_THRESHOLD);
+                                                                RAND_COLOR_THRESHOLD,
+                                                                RAND_COLOR_THRESHOLD,
+                                                                RAND_COLOR_THRESHOLD);
 
+                    //switch direction
                     if(pattern_data->direction[i] == -1)
                     {
                         pattern_data->direction[i] = 1;
@@ -181,6 +197,7 @@ void led_update_piano_war(led_update_function_data_t *data)
                 }
                 else
                 {
+                    //LED is dead; erase it
                     pattern_data->occupied[i] = 0;
                     pattern_data->colors[i] = 0;
                     pattern_data->direction[i] = 0;
@@ -190,15 +207,18 @@ void led_update_piano_war(led_update_function_data_t *data)
             {
                 if(pattern_data->occupied[i + pattern_data->direction[i]])
                 {
+                    //LED collision case
                     pattern_data->size[i]--;
 
                     if(pattern_data->size[i])
                     {
+                        //LED is alive; change color
                         pattern_data->colors[i] = random_near_color(pattern_data->colors[i],
-                                                                       RAND_COLOR_THRESHOLD,
-                                                                       RAND_COLOR_THRESHOLD,
-                                                                       RAND_COLOR_THRESHOLD);
+                                                                    RAND_COLOR_THRESHOLD,
+                                                                    RAND_COLOR_THRESHOLD,
+                                                                    RAND_COLOR_THRESHOLD);
 
+                        //switch direction
                         if(pattern_data->direction[i] == -1)
                         {
                             pattern_data->direction[i] = 1;
@@ -210,6 +230,7 @@ void led_update_piano_war(led_update_function_data_t *data)
                     }
                     else
                     {
+                        //LED is dead; erase it
                         pattern_data->occupied[i] = 0;
                         pattern_data->colors[i] = 0;
                         pattern_data->direction[i] = 0;
@@ -217,13 +238,11 @@ void led_update_piano_war(led_update_function_data_t *data)
                 }
                 else
                 {
+                    //move case: shift everything by one in the direction of travel
                     pattern_data->occupied[i + pattern_data->direction[i]] = 1;
-                    pattern_data->colors[i +
-                                         pattern_data->direction[i]] = pattern_data->colors[i];
-                    pattern_data->direction[i +
-                                            pattern_data->direction[i]] = pattern_data->direction[i];
-                    pattern_data->size[i +
-                                       pattern_data->direction[i]] = pattern_data->size[i];
+                    pattern_data->colors[i + pattern_data->direction[i]] = pattern_data->colors[i];
+                    pattern_data->direction[i + pattern_data->direction[i]] = pattern_data->direction[i];
+                    pattern_data->size[i + pattern_data->direction[i]] = pattern_data->size[i];
                     pattern_data->locked[i + pattern_data->direction[i]] = 1;
 
                     pattern_data->occupied[i] = 0;
@@ -235,6 +254,7 @@ void led_update_piano_war(led_update_function_data_t *data)
         }
     }
 
+    //add new LEDs
     int left_count = 0;
     int right_count = 0;
 
@@ -244,10 +264,12 @@ void led_update_piano_war(led_update_function_data_t *data)
 
         if(data->buffer[0] == 144)
         {
+            //pipe contained a new key
             pipe_pop(data->data_pipe, data->buffer, 2);
 
             if(data->buffer[1] > 0)
             {
+                //LEDs from left keys come from left, right keys come from right
                 if(data->buffer[0] - 21 < 44)
                 {
                     left_count++;
@@ -260,6 +282,7 @@ void led_update_piano_war(led_update_function_data_t *data)
         }
     }
 
+    //create new LEDs
     if(left_count > 0)
     {
         pattern_data->occupied[0] = 1;
@@ -276,6 +299,7 @@ void led_update_piano_war(led_update_function_data_t *data)
         pattern_data->size[LED_COUNT - 1] = right_count;
     }
 
+    //copy data into led_states[]
     for(int i = 0; i < LED_COUNT; i++)
     {
         data->led_states[i] = pattern_data->colors[i];
--- src/led_patterns/piano.h
+++ src/led_patterns/piano.h
@@ -3,10 +3,26 @@
 
 #include "core.h"
 
-void *new_led_update_piano_normal_data();
-void *new_led_update_piano_war_data();
+/**
+ * Function to create and initialize a new led_update_piano_normal_data_t
+ * @return A pointer to the new led_update_piano_normal_data_t
+ */
+void *new_led_update_piano_normal_data_t();
 
+/**
+ * Function to create and initialize a new led_update_piano_war_data_t
+ * @return A pointer to the new led_update_piano_war_data_t
+ */
+void *new_led_update_piano_war_data_t();
+
+/**
+ * The normal piano-enabled LED update function. Each key lights up one specific LED
+ */
 void led_update_piano_normal(led_update_function_data_t *);
+
+/**
+ * The war piano-enabled LED update. Each key sends a burst of light from one end
+ */
 void led_update_piano_war(led_update_function_data_t *);
 
 #endif //SMART_LEDS_LED_PATTERNS_H
